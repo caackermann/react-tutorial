5a3d154088294847683879d5b5b889ab0009e18b
Christian Ackermann <cackermann@3a.cl>
2019-03-06 17:16:06 -0300

    deleted resources buttons

--- src/components/resource.jsx
+++ src/components/resource.jsx
@@ -12,15 +12,6 @@ class Resource extends Component {
         <td>{this.props.resource.year} </td>
         <td>{this.props.resource.color}</td>
         <td>{this.props.resource.pantone_value}</td>
-
-        <td>
-          <Button color="success" size="sm" className="m-2">
-            View
-          </Button>
-          <Button color="danger" size="sm">
-            Delete
-          </Button>
-        </td>
       </tr>
     );
   }
